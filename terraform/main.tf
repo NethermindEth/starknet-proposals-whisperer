2a60dd9940a0f1dc1d3e90413433d929d695d2e6
AntiD2ta <mtpotro41@gmail.com>
2023-11-03 13:46:34 +0100

    fix: Prohibited resources statement

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "lambda_policy" {
       "logs:CreateLogStream",
       "logs:PutLogEvents"
     ]
-    resources = ["*"]
+    resources = ["arn:aws:logs:*:*:*"]
   }
 }
 
