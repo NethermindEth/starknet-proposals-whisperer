ac41aa96e08970bdcc192f784e596fe8559d6977
AntiD2ta <mtpotro41@gmail.com>
2023-11-03 13:42:32 +0100

    fix: Update Lambda Role name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "project_name" {
   type = string
   description = "The name of the project"
-  default = "starkent-proposals-whisperer"
+  default = "starknet-proposals-whisperer"
 }
 
 variable "iam_role_name" {
   type = string
   description = "The name of the IAM role"
-  default = "StarknetProposalWhispererRole"
+  default = "StarknetProposalWhispererLambdaRole"
 }
